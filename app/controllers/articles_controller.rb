6dc907513c80634b72af79a5f1f1fd5b292c0b6c
Martin SAMEK <martinsamek51@gmail.com>
2014-03-18 09:52:20 +0100

    render with partial article and partial comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,23 +5,17 @@ class ArticlesController < ApplicationController
   # GET /articles.json
   def index
     @articles = Article.all
-
-
-
   end
 
   # GET /articles/1
   # GET /articles/1.json
   def show
-
     @article = Article.find(params[:id])
-
   end
 
   # GET /articles/new
   def new
     @article = Article.new
-
   end
 
   # GET /articles/1/edit
